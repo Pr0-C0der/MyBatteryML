a3310f308cdd595fe1998679a3bca91d0fc5f080
Pr0-C0der <prathamesh.gadekar@hotmail.com>
2025-09-28 13:37:28 +0530

    adding code for plotting cycle plots for everything

--- plot_everything_correlation.ps1
+++ plot_everything_correlation.ps1
@@ -1,6 +1,6 @@
-python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/MATR --output_dir correlation_MATR --verbose
-python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/CALCE --output_dir correlation_CALCE --verbose
 python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/SNL     --output_dir correlation_SNL --verbose
 python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/HUST    --output_dir correlation_HUST --verbose
 python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/RWTH    --output_dir correlation_RWTH --verbose
 python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/UL_PUR  --output_dir correlation_UL_PUR --verbose
+python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/CALCE --output_dir correlation_CALCE --verbose
+python -m batteryml.data_analysis.correlation_mod --data_path data/preprocessed/MATR --output_dir correlation_MATR --verbose
---
+++ plot_everything_cycle_plots.ps1
@@ -0,0 +1,6 @@
+python -m batteryml.data_analysis.cycle_plotter_mod --data_path data/preprocessed/SNL     --output_dir cycle_plots_SNL
+python -m batteryml.data_analysis.cycle_plotter_mod --data_path data/preprocessed/HUST    --output_dir cycle_plots_HUST
+python -m batteryml.data_analysis.cycle_plotter_mod --data_path data/preprocessed/RWTH    --output_dir cycle_plots_RWTH
+python -m batteryml.data_analysis.cycle_plotter_mod --data_path data/preprocessed/UL_PUR  --output_dir cycle_plots_UL_PUR
+python -m batteryml.data_analysis.cycle_plotter_mod --data_path data/preprocessed/CALCE  --output_dir cycle_plots_CALCE
+python -m batteryml.data_analysis.cycle_plotter_mod --data_path data/preprocessed/MATR  --output_dir cycle_plots_MATR
